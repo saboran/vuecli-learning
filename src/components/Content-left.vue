3691e742003f2923f44f6994aeb58b41fc4e799f
linganmin <saboran@163.com>
2016-10-05 16:57:29 +0800

    add newslist and

--- src/components/Content-left.vue
+++ src/components/Content-left.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="col-md-8">
-    <div class="container-fluid" style="padding:0;">
+    <div class="container-fluid" style="padding:0;"  v-for="list of lists">
       <div class="row info-content">
         <div class="col-md-5 col-sm-5 col-xs-5">
-          <img src="http://ww2.sinaimg.cn/mw690/6aedb651gw1f8b6tykefej21bd0qo0zm.jpg" class="img-responsive" alt="">
+          <img :src="list.imageLink" :alt="list.title" style="width: 100%">
         </div>
         <div class="col-md-7 col-sm-7 col-xs-7">
-          <h4><a href="#">这里是标题</a></h4>
-          <p class="hidden-xs">描述描述</p>
-          <p><i class="fa fa-clock-o"></i> 2016.10.04</p>
+          <h4><a href="#">{{list.title}}</a></h4>
+          <p class="hidden-xs">{{list.content}}</p>
+          <p><i class="fa fa-clock-o"></i> {{list.createdTime}}</p>
         </div>
       </div>
     </div>
@@ -20,7 +20,50 @@
 export default {
   data () {
     return {
-      msg: 'contents'
+      lists: [
+        {'id': 1,
+        'title': '不说',
+        'content': '我们常劝人说，挺过去，会好起来的。其实心里知道就算不好起来，日子还是要一样的过。生活中还会有那么多快乐与伤悲，但不同的是我的生命中曾有你，你给予过的温暖已被我牢牢记在心里，所以没关系。',
+        'createdTime': '2016-10-10',
+        'link': 'http://www.linganmin.cn',
+        'imageLink': 'http://ww2.sinaimg.cn/mw690/6aedb651gw1f8b6tykefej21bd0qo0zm.jpg'
+        },
+        {'id': 1,
+        'title': '不说',
+        'content': '我们常劝人说，挺过去，会好起来的。其实心里知道就算不好起来，日子还是要一样的过。生活中还会有那么多快乐与伤悲，但不同的是我的生命中曾有你，你给予过的温暖已被我牢牢记在心里，所以没关系。',
+        'createdTime': '2016-10-10',
+        'link': 'http://www.linganmin.cn',
+        'imageLink': 'http://ww2.sinaimg.cn/mw690/6aedb651gw1f8b6tykefej21bd0qo0zm.jpg'
+        },
+        {'id': 1,
+        'title': '不说',
+        'content': '我们常劝人说，挺过去，会好起来的。其实心里知道就算不好起来，日子还是要一样的过。生活中还会有那么多快乐与伤悲，但不同的是我的生命中曾有你，你给予过的温暖已被我牢牢记在心里，所以没关系。',
+        'createdTime': '2016-10-10',
+        'link': 'http://www.linganmin.cn',
+        'imageLink': 'http://ww2.sinaimg.cn/mw690/6aedb651gw1f8b6tykefej21bd0qo0zm.jpg'
+        },
+        {'id': 1,
+        'title': '不说',
+        'content': '我们常劝人说，挺过去，会好起来的。其实心里知道就算不好起来，日子还是要一样的过。生活中还会有那么多快乐与伤悲，但不同的是我的生命中曾有你，你给予过的温暖已被我牢牢记在心里，所以没关系。',
+        'createdTime': '2016-10-10',
+        'link': 'http://www.linganmin.cn',
+        'imageLink': 'http://ww2.sinaimg.cn/mw690/6aedb651gw1f8b6tykefej21bd0qo0zm.jpg'
+        },
+        {'id': 1,
+        'title': '不说',
+        'content': '我们常劝人说，挺过去，会好起来的。其实心里知道就算不好起来，日子还是要一样的过。生活中还会有那么多快乐与伤悲，但不同的是我的生命中曾有你，你给予过的温暖已被我牢牢记在心里，所以没关系。',
+        'createdTime': '2016-10-10',
+        'link': 'http://www.linganmin.cn',
+        'imageLink': 'http://ww2.sinaimg.cn/mw690/6aedb651gw1f8b6tykefej21bd0qo0zm.jpg'
+        },
+        {'id': 1,
+        'title': '不说',
+        'content': '我们常劝人说，挺过去，会好起来的。其实心里知道就算不好起来，日子还是要一样的过。生活中还会有那么多快乐与伤悲，但不同的是我的生命中曾有你，你给予过的温暖已被我牢牢记在心里，所以没关系。',
+        'createdTime': '2016-10-10',
+        'link': 'http://www.linganmin.cn',
+        'imageLink': 'http://ww2.sinaimg.cn/mw690/6aedb651gw1f8b6tykefej21bd0qo0zm.jpg'
+        }
+      ]
     }
   }
 }
@@ -46,5 +89,12 @@ export default {
   }
   p{
     color: gray;
+  }
+  h4 > a{
+    text-decoration: none;
+    color: gray;
+  }
+  h4 > a:hover{
+    color: #41B883;
   }
 </style>
